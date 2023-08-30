b5054d0816231d9ef62935bf3168be03caa73be7
Anthony Castillo <anthony1093ca@gmail.com>
2023-08-30 09:36:19 -0700

    Added a sample class with basic functions.

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 # testing a push
 import time
 print(time.ctime())
-<<<<<<< HEAD
+while range(5):
+    print("I love programming!!!")
 
 class ThisIsAClass():
     def __init__(self):
@@ -13,13 +14,6 @@ class ThisIsAClass():
     def __str__(self):
         pass
 
-    def customFunction(self):
+    def aCustonFunction(self):
         pass
-
-    def etc(self):
-        pass
-# End ThisIsAClass
-=======
-while True:
-    print("I love programming!!!")
->>>>>>> ff3a57c5a57fa290827f03a43c73aa7cea19dc69
+# end ThisIsAClass
