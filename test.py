c241f21299d4fe4741f51df52443c2b6d6afa18a
Anthony Castillo <anthony1093ca@gmail.com>
2023-08-30 09:23:12 -0700

    Adding a sample Class

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 # testing a push
 import time
 print(time.ctime())
+
+class ThisIsAClass():
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def customFunction(self):
+        pass
+
+    def etc(self):
+        pass
+# End ThisIsAClass
