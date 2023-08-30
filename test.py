0be3bb0b2107abdde19ab425ee6df9d340b65ece
Anthony C <57550910+CastilloAnthony@users.noreply.github.com>
2023-08-30 11:43:05 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 # testing a push
 import time
 print(time.ctime())
+
 while range(5):
     print("I love programming!!!")
 
-class ThisIsAClass():
-    def __init__(self):
+class ThisIsAClass(): # An example class for how all other classes should look like.
+    def __init__(self): # Initialize the class
         pass
 
-    def __del__(self):
+    def __del__(self): # Delete the variables of the class
         pass
 
-    def __str__(self):
+    def __str__(self): # Print the data of the class
         pass
 
-    def aCustonFunction(self):
+    def _aPrivateFunction(self): # A private function to be accessed only by this class itself
+        pass
+        
+    def aPublicFunction(self): # A public function meant to be called from outside of the class 
         pass
 # end ThisIsAClass
