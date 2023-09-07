81bda27f947a6eeb655e81751b0dd236b97b9cca
Anthony Castillo <anthony1093ca@gmail.com>
2023-09-06 22:41:37 -0700

    Updated README; Added secondary functions

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# CS-4800_Server-Status-Checker
-This program is designed for California State University Stanislaus' CS-4800 2023 Fall class. It is meant to be able to ping servers to check to see if they are up (responsive) or if they are down (unresponsive). The program will ping based on a user given or predetermined poll rate. The status of the server is then saved to a file alongside a timestamp which will be used later to display a histogram of each indvidual server's status.
+# CS-4800_Project_Sherlock
+This program is designed for California State University Stanislaus' CS-4800 2023 Fall class. It is meant to be able to ping servers to check to see if they are up (responsive) or if they are down (unresponsive). The program will ping based on a user given or predetermined poll rate. The status of the server is then saved to a file alongside a timestamp which will be used later to display a histogram of each indvidual server's status.  This program is hosted on the local machine and will require an internet conneciton to be useful, however, it is also capable of pinging device on the Local Area Network (LAN) and Wide Area Network (WAN).
 
 ## Team Members
 - Christian Alameda
@@ -8,9 +8,13 @@ This program is designed for California State University Stanislaus' CS-4800 202
 - Vel Perez-Barba
 
 ## Capabilities
-- Accept user given IP addresses and Website
-- Store given IP addresses for future examination
-- Poll IP addresses, websites, and servers every (elapsed period) and stores that info
-- Display an uptime/downtime histogram, pie chart, or using some other means
-- A webui will be used to manage the program.
-- TBD
+- Runs locally
+- Accept user given IP addresses, domain names, and/or websites
+- Stores given IP addresses in a database
+- Polls those addresses every (user defiend elapsed period) and stores latency, downtime and uptime info
+- Display an uptime/downtime histogram
+- A webui is used to manage the program
+- Can trace connections to see where the connection ends if it is unable to reach its destination.
+
+### Disclaimer
+- All aspects are subject to change as the project progresses.
--- exampleProject.py
+++ exampleProject.py
@@ -24,36 +24,44 @@ class Server():
     def __del__(self): # Delete the variables of the class
         pass
 
-    def _initializeNetwork(self):
+    def _initializeNetwork(self): # CORE
         # What Networking Adapters are available on this device? Networking Adapters (i.e., ethernet, wireless adapter, bluetooth?, etc.)
         # Which do we want to use?
         # What ports are we going to use? 5555? 7777?
         pass
 
-    def _initializeDatabase(self):
+    def _initializeDatabase(self): # CORE
         # Starts the database server.
         # SQL? MySQL? MongoDB? CSV? TXT? Excel Spreadsheet?
         pass
 
-    def _connectToDatabase(self):
+    def _connectToDatabase(self): # CORE
         # Connects to the database.
         pass
 
-    def _pollServers(self):
+    def _pollServers(self): # CORE
         # What polling rate do we want to use?
         # Needs to be able to continuously poll servers regardless of anything else happening (i.e., use multiprocessing or threads).
         pass
 
-    def _addDataToDatabase(self):
+    def _scrapeSocialMedia(self): # SECONDARY
+        # Scrapes twitter/x and similar sites for information on planned maintenance, estimated recovery times, etc.
+        pass
+
+    def _makePredictions(self): # SECONDARY
+        # Uses the history of currently monitored addresses and sites to make a prediction for when a site will go down, or how soon it will recover.
+        pass
+
+    def _addDataToDatabase(self): # CORE
         # Takes data gathered from _pollServers() and sends it into the database.
         pass
 
-    def _checkClientMessages(self):
+    def _checkClientMessages(self): # CORE
         # Should be able to continuously check for messages from clients regardless of anything else happening (i.e., use multiprocessing or threads).
         # Calls _getDataFromDatabase() whenever a client requests data.
         pass
 
-    def _addNewClient(self):
+    def _addNewClient(self): # CORE
         # Assigns a new client a guid and adds that client to the list of identified clients.
         # Are we using private/public key authentication?
         # newClient = {'guid':'6B29FC40-CA47-1067-B31D-00DD010662DA', 'ip':'10.0.0.30', 'name':'Anthony's Phone', 'key':'randomDigits'}
@@ -61,11 +69,11 @@ class Server():
         # etc.
         pass
 
-    def _getDataFromDatabase(self):
+    def _getDataFromDatabase(self): # CORE
         # As a reaction to any clients requesting data.
         pass
 
-    def _sendDataToClient(self, clientIp):
+    def _sendDataToClient(self, clientIp): # CORE
         pass
 
     def start(self):
@@ -80,25 +88,25 @@ class Client():
     def __del__(self): # Delete the variables of the class.
         pass
 
-    def _initializeNetwork(self):
+    def _initializeNetwork(self): # CORE
         # What Networking Adapters are available on this device? (i.e., ethernet, wireless adapter, bluetooth?, etc.).
         # Which do we want to use?
         pass
     
-    def _generatePrivatePublicKey(self):
+    def _generatePrivatePublicKey(self): # CORE
         # Generates private key then generates public key for authentication purposes.
         pass
 
-    def _connectToServer(self, serverIp):
+    def _connectToServer(self, serverIp): # CORE
         # Performs an authentication handshake with the server.
         # Sends some identifier information to the server (name of device, public key).
         pass
 
-    def _requestDataFromServer(self, serverIp): 
+    def _requestDataFromServer(self, serverIp): # CORE
         # Request a Snapshot of the last 3 hours? 24 hours? 7 days? 1 month?
         pass
     
-    def _gui(self):
+    def _gui(self): # CORE
         # Displays the data retrieved from _requestDataFromServer().
         pass
 
