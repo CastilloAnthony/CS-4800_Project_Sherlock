90fe0ebea127af95a0466e2ebb292dea8268d0f6
Anthony Castillo <anthony1093ca@gmail.com>
2023-10-11 09:43:01 -0700

    Adding client.py and server.py

---
+++ client.py
@@ -0,0 +1,32 @@
+class Homepage():
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def trackWebsite(self):
+        pass
+
+    def deleteWebsite(self):
+        pass
+
+    def addPreset(self):
+        pass
+
+    def editPreset(self):
+        pass
+
+    def deletePreset(self):
+        pass
+# end Homepage
+
+class ConnectionAgent():
+    def __init__(self):
+        pass
+
+class EditPreset():
+    def __init__(self):
+        pass
+#end EditPreset
+
