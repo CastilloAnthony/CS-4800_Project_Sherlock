1898f8e36a461598b40be761293b703c680f2984
Anthony Castillo <anthony1093ca@gmail.com>
2023-10-12 11:57:08 -0700

    Added classes and functions for the server.

--- server.py
+++ server.py
@@ -5,23 +5,27 @@ import multiprocessing # For concurrent processing (client requests, data retriv
 import pymongo
 
 class Server(): # The main server handler class
-    # Most new processes should be created as a new process
     def __init__(self):
-        self._startDB()
-        self.__DBconneciton = DBConnectionAgent()
-    
+        self.__DBconneciton = False
+        self.__clientManager = False
     def __del__(self):
         pass
 
-    def _startDB(self):
+    def startDB(self):
+        """Creates the database and then sets up a conneciton agent
+        """
         pass
 
-    def _setupConnection(self):
-        pass
+    def setupConnection(self, address="localhost", port="27017"):
+        self.__DBconneciton = DBConnectionAgent()
+        if self.__DBconneciton.connect(address, port):
+            print("Successfully connected to DB at "+"mongodb://"+address+":"+port+"/")
+            self.__clientManager = ClientListener()
+        else:
+            print("Unable to connect to DB at "+"mongodb://"+address+":"+port+"/")
     
     def _clientListener(self):
         pass
-
 # end Server
 
 class DBConnectionAgent():
@@ -33,29 +37,66 @@ class DBConnectionAgent():
         del self.__client
         del self.__db
 
-    def connect(self, dbAddress="localhost", port="27017"):
-        """Connects to a specified server.
+    def connect(self, address="localhost", port="27017"):
+        """Connects to a specified server
 
         Args:
-            dbAddress (string): The IP Address of the server. Defaults to "localhost".
-            port (string): The Port Number for the Server. Defaults to "27017".
+            address (string): The IP Address of the server. Defaults to "localhost"
+            port (string): The Port Number for the Server. Defaults to "27017"
+
+        Returns:
+            bool: Success/Failure to connect
         """
         try:
-            self.__client = pymongo.MongoClient("mongodb://"+dbAddress+":"+port+"/")
+            self.__client = pymongo.MongoClient("mongodb://"+address+":"+port+"/")
+            return True
         except:
-            print("Could not connect to DB at: "+"mongodb://"+dbAddress+":"+port+"/")
+            print("Could not connect to DB at: "+"mongodb://"+address+":"+port+"/")
             self.__client = False
+            return False
         
     def getDBs(self):
+        """Returns a List of databases for the connected system
+
+        Returns:
+            list: Returns a List of databases for the connected system in the format of ["admin", "config", "local"]
+        """
         if self.__client != False:
             return self.__client.list_database_names()
 
     def useDB(self, db):
-        self.__db = self.__client[db]
+        """Sets the target DB
+
+        Args:
+            db (string): The DB to use, must be found inside of the getDBs() function
+
+        Returns:
+            bool: Success/Failure to use the specified DB
+        """
+        if db in self.getDBs():
+            self.__db = self.__client[db]
+            return True
+        else:
+            return False
 
     def addToDB(self, column, content):
+        """Adds data into the DB at the specified column
+
+        Args:
+            column (string): The column we wish to add data into
+            content (unspecified): The content to add into the DB
+
+        Returns:
+            bool: Success/Failure to add content to the DB
+        """
         if self.__db != False:
-            self.__db[column].insert_one(content)
+            try:
+                self.__db[column].insert_one(content)
+                return True
+            except:
+                return False
+        else:
+            return False
 
     def removeFromDB(self, column, content):
         pass
@@ -63,12 +104,72 @@ class DBConnectionAgent():
     def requestFromDB(self, column):
         if self.__db != False:
             pass
-#end dbConnecitonAgent
+#end DBConnecitonAgent
+
+class ClientListener():
+    # Functions should use multiprocessing for optimized response time
+    def __init__(self):
+        self.__knownClients = {}
+        self.__processList = {}
+
+    def __del__(self):
+        pass
+
+    def _listen(self):
+        while True:
+            break
+
+    def _verifyClient(self, client):
+        if client in self.__knownClients:
+            return True
+        else:
+            return False
+
+    def _addClient(self, client):
+        if not self._verifyClient(client):
+            newClient = Client()
+            newClient.setName("test")
+            newClient.setAddress("1.2.3.4")
+            newClient.setUUID(uuid.uuid4())
+            self.__knownClients.append(newClient)
+
+    def _ServerToClient(self, client):
+        pass
+
+    def _ClientToServer(self):
+        pass
+
+    def _processManager(self):
+        pass
+#end CLientListener
+
+class Client():
+    def __init__(self):
+        self.__name = False
+        self.__address = False
+        self.__uuid = False
+
+    def __del__(self):
+        pass
+
+    def setName(self, name):
+        self.__name = name
+
+    def setAddress(self, address):
+        self.__address = address
+
+    def setUUID(self, uuid):
+        self.__uuid = uuid
+
+    def getClientInfo(self):
+        return {"name":self.__name, "address":self.__address, "uuid":self.__uuid}
+#end Client
 
 def main():
     newServer = Server()
-    #newServer.
+    newServer.startDB()
+    newServer.setupConnection()
 
 if __name__ == "__main__":
     main()
-#end main()
+#end main
