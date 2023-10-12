22594d98922eb299ffe1b5ccbb132c37a2d0a47d
Anthony Castillo <anthony1093ca@gmail.com>
2023-10-12 11:05:10 -0700

    Added a Conneciton Agent for the server to DB interactions

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
-import mysql.connector # For database, unless we use MongoDB
-import sql # For transferring data between server and client
+#import mysql.connector # For database, unless we use MongoDB
+#import sql # For transferring data between server and client
 import uuid # Universally Unique IDentifier (For User IDs and others things)
 import multiprocessing # For concurrent processing (client requests, data retrival, etc.)
+import pymongo
 
 class Server(): # The main server handler class
     # Most new processes should be created as a new process
     def __init__(self):
         self._startDB()
-        pass
+        self.__DBconneciton = DBConnectionAgent()
     
     def __del__(self):
         pass
@@ -22,3 +23,52 @@ class Server(): # The main server handler class
         pass
 
 # end Server
+
+class DBConnectionAgent():
+    def __init__(self):
+        self.__client = False
+        self.__db = False
+
+    def __del__(self):
+        del self.__client
+        del self.__db
+
+    def connect(self, dbAddress="localhost", port="27017"):
+        """Connects to a specified server.
+
+        Args:
+            dbAddress (string): The IP Address of the server. Defaults to "localhost".
+            port (string): The Port Number for the Server. Defaults to "27017".
+        """
+        try:
+            self.__client = pymongo.MongoClient("mongodb://"+dbAddress+":"+port+"/")
+        except:
+            print("Could not connect to DB at: "+"mongodb://"+dbAddress+":"+port+"/")
+            self.__client = False
+        
+    def getDBs(self):
+        if self.__client != False:
+            return self.__client.list_database_names()
+
+    def useDB(self, db):
+        self.__db = self.__client[db]
+
+    def addToDB(self, column, content):
+        if self.__db != False:
+            self.__db[column].insert_one(content)
+
+    def removeFromDB(self, column, content):
+        pass
+
+    def requestFromDB(self, column):
+        if self.__db != False:
+            pass
+#end dbConnecitonAgent
+
+def main():
+    newServer = Server()
+    #newServer.
+
+if __name__ == "__main__":
+    main()
+#end main()
