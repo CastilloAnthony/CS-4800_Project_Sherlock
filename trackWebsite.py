04c7624062fd06f49f5db1b377588edbfdadecd0
ChristianAlameda <jazzbilbo@gmail.com>
2023-10-13 15:26:28 -0700

    finiishing touches on enterWebsite()

--- trackWebsite.py
+++ trackWebsite.py
@@ -12,38 +12,25 @@ class TrackWebsite(): # Controller(or Boundary?)
     
     def enterWebsite(self):
         # Needs a url
-        prefixList = ["HTTPS://, HTTP://"]
-        extensionList = [".COM", 
-                         ".NET", 
-                         ".ORG", 
-                         "BIZ", 
-                         ".GOV", 
-                         ".INFO", 
-                         ".EDU", 
-                         ".TV",
-                         ]
-        
-        prefixBool = True
-        extensionBool = True
         url = ''
-        
-         
-        url = input("Enter url\nExample: https://google.com\nURL: ")
-        parsedUrl = urlparse(url)
-        
-        
-        #SPECIAL THANKS TO Adem Öztaş from Stack Overflow
-        response = requests.get(url)
-        if response.status_code == 200:
-            print('Web site exists')
-        else:
-            print('Web site does not exist') 
+        WebsiteDoesNotExist = True
+        while WebsiteDoesNotExist:
+            url = input("Enter url\nExample: https://google.com\nURL: ")
+            # parsedUrl = urlparse(url)
             
-    
+            #SPECIAL THANKS TO Adem Öztaş from Stack Overflow
+            response = requests.get(url)
+            if response.status_code == 200:
+                print('Web site exists')
+                WebsiteDoesNotExist = False
+            else:
+                print('Web site does not exist') 
+                WebsiteDoesNotExist = True
+            
         dict = {"url":url}
         
         self.database.insertPost(dict)
-    #end TrackWebsite
+        print("inserted\n", url, "\nsuccessfully")
 
 track = TrackWebsite()
 track.enterWebsite()
