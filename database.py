9046b0a6308a9d406c9824122fb8e01a2ecb3fc8
ChristianAlameda <jazzbilbo@gmail.com>
2023-10-13 10:05:55 -0700

    hello

--- database.py
+++ database.py
@@ -1,45 +1,50 @@
 # use website: https://www.mongodb.com/try/download/community
 # download mongodb version 7.0
-
 import pymongo
+def main():
+    
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-mycol = mydb["customers"]
+    # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    # mydb = myclient["mydatabase"]
+    # mycol = mydb["customers"]
 
-mydict = { "name": "John", "address": "Highway 37" }
+    # mydict = { "name": "John", "address": "Highway 37" }
 
-x = mycol.insert_one(mydict)
-
-
-dblist = myclient.list_database_names()
-if "mydatabase" in dblist:
-  print("The database exists.")
-  
-mydict = { "name": "Peter", "address": "Lowstreet 27" }
-
-x = mycol.insert_one(mydict)
-
-print(x.inserted_id)
-
-mylist = [
-  { "name": "Amy", "address": "Apple st 652"},
-  { "name": "Hannah", "address": "Mountain 21"},
-  { "name": "Michael", "address": "Valley 345"},
-  { "name": "Sandy", "address": "Ocean blvd 2"},
-  { "name": "Betty", "address": "Green Grass 1"},
-  { "name": "Richard", "address": "Sky st 331"},
-  { "name": "Susan", "address": "One way 98"},
-  { "name": "Vicky", "address": "Yellow Garden 2"},
-  { "name": "Ben", "address": "Park Lane 38"},
-  { "name": "William", "address": "Central st 954"},
-  { "name": "Chuck", "address": "Main Road 989"},
-  { "name": "Viola", "address": "Sideway 1633"}
-]
-
-x = mycol.insert_many(mylist)
-
-#print list of the _id values of the inserted documents:
-print(x.inserted_ids)
+    # x = mycol.insert_one(mydict)
 
 
+    # dblist = myclient.list_database_names()
+    # if "mydatabase" in dblist:
+    # print("The database exists.")
+    
+    # mydict = { "name": "Peter", "address": "Lowstreet 27" }
+
+    # x = mycol.insert_one(mydict)
+
+    # print(x.inserted_id)
+
+    # mylist = [
+    # { "name": "Amy", "address": "Apple st 652"},
+    # { "name": "Hannah", "address": "Mountain 21"},
+    # { "name": "Michael", "address": "Valley 345"},
+    # { "name": "Sandy", "address": "Ocean blvd 2"},
+    # { "name": "Betty", "address": "Green Grass 1"},
+    # { "name": "Richard", "address": "Sky st 331"},
+    # { "name": "Susan", "address": "One way 98"},
+    # { "name": "Vicky", "address": "Yellow Garden 2"},
+    # { "name": "Ben", "address": "Park Lane 38"},
+    # { "name": "William", "address": "Central st 954"},
+    # { "name": "Chuck", "address": "Main Road 989"},
+    # { "name": "Viola", "address": "Sideway 1633"}
+    # ]
+
+    # x = mycol.insert_many(mylist)
+
+    # #print list of the _id values of the inserted documents:
+    # print(x.inserted_ids)
+    
+    # return x
+    
+    
+
+main()
