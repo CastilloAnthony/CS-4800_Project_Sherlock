bd4524f7e10fe151cfd56812d90a74f3f85e200f
ChristianAlameda <jazzbilbo@gmail.com>
2023-10-14 11:47:58 -0700

    php

--- exampleCode/practice.php
+++ exampleCode/practice.php
@@ -1,56 +1,48 @@
 <?php
-    echo "Hey, this is christian";
+    // echo "Hey, this is christian";
     //This is a comment
     /*
     This is a multy line comment
     */
-    $a = 10; //setting variable named 'a' to integer 10;
-    $b = 'apples'; //setting variable 'b' to string apples
-    echo $a;
-    echo "<br>";
-    echo $b;
-    $b = 12;
-    $r = $a +$b;
-    echo "this is the result: " . $r . "apples";
-    if ($a<$b){
-        echo "a is greate than b";
-    }
-    elseif($a == $b){
-        echo "a is equal to b";
-    }
-    else{
-        echo "a is smaller than b";
-    }
-
-    $index_array = ['a','b','c','d',1234,1.2];
-    //this is an index array
-    echo $index_array[1];
-    $associative_array = ["pencils"=>4, "pens"=>5, "erasers"=>10]
-    echo $associative_array["pens"];
-    for($i=0;$i<20;$i++){
-        echo $i;
-    }
-    while($a<$b){
-        echo $a;
-        a++
-    }
-    foreach($associative_array as $ar){
-        echo $ar; //4510
-    }
-    // if(isset($IS_GET['username'])){
-    //     echo "Hello" . $_GET['username'];
+    // $a = 10; //setting variable named 'a' to integer 10;
+    // $b = 'apples'; //setting variable 'b' to string apples
+    // echo "<br>";
+    // echo $a . " " . $b;
+    // $b = 12;
+    // $r = $a +$b;
+    // echo "this is the result: " . $r . "apples";
+    // if ($a<$b){
+    //     echo "a is greate than b";
     // }
-    
-    // if(isset($IS_POST['username'])){
-    //     echo "Hello" . $_POST['username'];
+    // elseif($a == $b){
+    //     echo "a is equal to b";
     // }
-    
-    // if(isset($IS_REQUEST['username'])){
-    //     echo "Hello" . $_REQUEST['username'];
+    // else{
+    //     echo "a is smaller than b";
     // }
 
-    function hello(){
-        echo "hey, this is christian";
-    }
-    hello()
+    // $index_array = ['a','b','c','d',1234,1.2];
+    // //this is an index array
+    // echo $index_array[1];
+    // $associative_array = ["pencils"=>4, "pens"=>5, "erasers"=>10];
+    // echo $associative_array["pens"];
+    // for($i=0;$i<20;$i++){
+    //     echo $i;
+    // }
+    // while($a<$b){
+    //     echo $a;
+    //     $a = $a + 1;
+    // }
+    // foreach($associative_array as $ar){
+    //     echo $ar; //4510
+    // }
+    //echo "Hello " . $_GET['username'];
+    //echo "Hello " . $_POST['username']; 
+    echo "Hello " . $_REQUEST['username'];
+    
+
+    // function hello(){
+    //     echo "hey, this is christian";
+    // }
+    // hello()
 ?>
