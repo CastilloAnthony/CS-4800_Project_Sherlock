692fa097c6dee8289e1f3801c93d3b3830fe9e8a
ChristianAlameda <jazzbilbo@gmail.com>
2023-10-13 22:07:04 -0700

    can't figure out how to run the php but it is valid php code if you can get it working

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewset" content="device-width,initial-scale=10">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="practice.php" method="GET">
+            <!--
+                GET: unsafe way to send info,
+                POST: safe way to send info,
+                REQUEST: if you don't know you used
+                        in the other php file,
+                COOKIE: 
+            -->
+            <input type="text" name="username">
+            <button type="submit">Click Me!!!</button>
+        </form>
+    </body>
+</html>
+
+
---
+++ practice.php
@@ -0,0 +1,56 @@
+<?php
+    echo "Hey, this is christian";
+    //This is a comment
+    /*
+    This is a multy line comment
+    */
+    $a = 10; //setting variable named 'a' to integer 10;
+    $b = 'apples' //setting variable 'b' to string apples
+    echo $a;
+    echo "<br>";
+    echo $b;
+    $b = 12;
+    $r = $a +$b;
+    echo "this is the result: " . $r . "apples";
+    if ($a<$b){
+        echo "a is greate than b";
+    }
+    elseif($a == $b){
+        echo "a is equal to b";
+    }
+    else{
+        echo "a is smaller than b";
+    }
+
+    $index_array = ['a','b','c','d',1234,1.2];
+    //this is an index array
+    echo $index_array[1];
+    $associative_array = ["pencils"=>4, "pens"=>5, "erasers"=>10]
+    echo $associative_array["pens"];
+    for($i=0;$i<20;$i++){
+        echo $i;
+    }
+    while($a<$b){
+        echo $a;
+        a++
+    }
+    foreach($associative_array as $ar){
+        echo $ar; //4510
+    }
+    // if(isset($IS_GET['username'])){
+    //     echo "Hello" . $_GET['username'];
+    // }
+    
+    // if(isset($IS_POST['username'])){
+    //     echo "Hello" . $_POST['username'];
+    // }
+    
+    // if(isset($IS_REQUEST['username'])){
+    //     echo "Hello" . $_REQUEST['username'];
+    // }
+
+    function hello(){
+        echo "hey, this is christian";
+    }
+    hello()
+?>
