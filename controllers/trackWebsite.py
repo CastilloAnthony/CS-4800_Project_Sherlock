efcd132fcdb6d441dd8f6d2e59596e0017a663e2
ChristianAlameda <jazzbilbo@gmail.com>
2023-10-19 10:22:56 -0700

    added some class to trackWebsite

--- controllers/trackWebsite.py
+++ controllers/trackWebsite.py
@@ -1,4 +1,8 @@
-class TrackWebsite(): # Controller
+#pip instal requests
+import requests
+#pip install icecream
+from icecream import ic
+class TrackWebsite: # Controller
     def __init__(self):
         #self.database = Database()
         #self.database.connect()
@@ -7,35 +11,51 @@ class TrackWebsite(): # Controller
     def __del__(self):
         pass
     
-    def enterWebsite(self):
-        # Needs a url
-        prefixList = ["HTTPS://, HTTP://"]
-        extensionList = [".COM", 
-                         ".NET", 
-                         ".ORG", 
-                         "BIZ", 
-                         ".GOV", 
-                         ".INFO", 
-                         ".EDU", 
-                         ".TV",
-                         ]
+    def enterWebsite(self, url):
         
-        prefixBool = True
-        extensionBool = True
-        url = ''
+        statuses = {
+            200: "Website Available",
+            301: "Permanent Redirect",
+            302: "Temporary Redirect",
+            404: "Not Found",
+            500: "Internal Server Error",
+            503: "Service Unavailable"
+        }
         
-        while prefixBool and extensionBool:
-            url = input("Enter url\nExample: https://google.com\nURL: ").upper()
+        
+        try:
+            web_response = requests.get(url, auth=('user','pass'))
+            url_info = {
+                'url':url,
+                'status_num':web_response.status_code,
+                'status_desc':statuses[web_response.status_code],
+                # 'url_json':web_response.json()
+            }
+            ic(url_info)
             
-            print(url)
-            for prefix in prefixList:
-                if prefix in url: prefixBool = False
-            for extension in extensionList:
-                if extension in url: extensionBool = False
-            else:
-                continue
-        print(url)
-        dict = {"url":url}
-        
-        #self.database.insertPost(dict)
-#end TrackWebsite
+                
+
+        except:
+            url_info = {
+                'url':url,
+                'status_num':web_response.status_code,
+                'status_desc':statuses[web_response.status_code],
+                # 'url_json':web_response.json()
+            }
+            ic(url_info)
+
+
+
+
+website_url = [
+        "https://www.google.co.in", 
+        "https://www.yahoo.com", 
+        "https://www.amazon.co.in", 
+        "https://www.pipsnacks.com/404",
+        "http://the-internet.herokuapp.com/status_codes/301",
+        "http://the-internet.herokuapp.com/status_codes/500"
+    ]
+x = TrackWebsite()
+for url in website_url:
+    # x.enterWebsite(i)
+    print(x.enterWebsite(url))
