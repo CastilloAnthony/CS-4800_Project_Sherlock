ff5cd9f493f86d6bedff22b8eda02d55aaef3e7a
spangy22 <109324199+spangy22@users.noreply.github.com>
2023-10-25 09:31:56 -0700

    updated graphTableGenerator.py

---
+++ controllers/graphTableGenerator.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime, timedelta
+import pytz
+
+filename = 'gg'
+
+#load files
+
+listOfHeaders = []
+#selectedData = listOfHeaders[##]
+#print('Selected Webpage(s): ', )
+
+#initialize timestamp
+#format for timestamp function: ??
+
+#need functions for uptime, downtime, and latency calculations
