e7afe62275911c8920ee6e85acf85ab67ae4f380
spangy22 <109324199+spangy22@users.noreply.github.com>
2023-10-31 08:08:41 -0700

    update to graphTableGenerator

--- controllers/graphTableGenerator.py
+++ controllers/graphTableGenerator.py
@@ -4,16 +4,24 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime, timedelta
 import pytz
+import requests
+from server import DBconnectionAgent
 
 filename = 'gg'
 
 #load files
+#need function to read requested websites from database
+#class DBconnectionAgent:
+    #self. 
 
-listOfHeaders = []
-#selectedData = listOfHeaders[##]
-#print('Selected Webpage(s): ', )
+#df = pd.read_#
+#for index, websites in df.iterrows():
+##
+#print(df)
 
 #initialize timestamp
 #format for timestamp function: ??
 
 #need functions for uptime, downtime, and latency calculations
+response = requests.get()
+print(response.status_code) #gives the status of whether or not a website is active/inactive
