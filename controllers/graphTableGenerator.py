426f4d5e40e9310c9c4d21c3af4ab786600d874b
spangy22 <109324199+spangy22@users.noreply.github.com>
2023-11-01 09:44:41 -0700

    added a time conversion function (in progress)

--- controllers/graphTableGenerator.py
+++ controllers/graphTableGenerator.py
@@ -32,7 +32,43 @@ class graphTableGenerator:
                 print(len(newData))
 
             # have the requested data print out in a list
+    
+    def timeConversion():
+        registeredStartDate = datetime()
+        GMT = [] # This our GMT_time 
+        for i in TimeDate: 
+            dt = datetime.strptime(i,"%d:%m:%Y %H:%M:%S") # Stripped information from each row 
+            GMT.append(dt)   
+            
+        # Convert GMT to Local Time
 
+        #PDT_time = []  
+        T = pd.Series(data=GMT) # Inputting GMT_time into a series 
+
+        # Converts GMT Dates and Times to Pacific Daylight Savings Time 
+        local_Date_time = T.dt.tz_localize('GMT').dt.tz_convert('America/Los_Angeles').dt.tz_localize(None) # This line converts time_zones
+
+        # Storing local dates and times into list  
+        PDT_Date_time = []
+        for i in local_Date_time: 
+            dt_objects = i.to_pydatetime() # Turns the timestamps to datetime objects 
+            PDT_Date_time.append(dt_objects)
+            
+        # Find the total seconds starting the week of april 4th 
+
+        PDT_time = [] # official PDT time 
+
+        for i in PDT_Date_time: # Takes time in 
+            
+            timediff = i.timestamp() - registeredStartDate.timestamp()
+            
+            PDT_time.append(timediff)
+            
+        print(PDT_Date_time[0])
+        print(PDT_Date_time[0]- registeredStartDate)
+        print(PDT_time[0])
+        print(abs(df[selectedData].mean())**2)
+        print(abs(np.mean(df[selectedData]))**2)
 
 
 
