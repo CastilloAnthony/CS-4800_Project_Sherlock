27ad20519ac50e12ca72e25a0d9f8eb035bfee4f
Anthony Castillo <anthony1093ca@gmail.com>
2023-11-01 09:13:01 -0700

    Added base layer of code for 3 new controllers

---
+++ controllers/viewPreset.py
@@ -0,0 +1,33 @@
+import time
+
+class ViewPreset():
+    def __init__(self, requestQ, dataQ):
+        self.__requestQ, self.__dataQ = requestQ, dataQ
+
+    def __del__(self):
+        del self.__requestQ, self.__dataQ
+
+    def requestData(self, request):
+        self.__requestQ.put(request)
+        time.sleep(0.1)
+        initialDataID = False
+        while self.__dataQ.empty() != True:
+            newData = self.__dataQ.get()
+            print(newData)
+            if newData['id'] == initialDataID:
+                self.__requestQ.put(request)
+                time.sleep(0.1) #import time
+                initialDataID = False
+            elif initialDataID == False:
+                initialDataID = newData['id']
+            if newData['id'] == request['id']:
+                if newData['data'] is not False:
+                    return newData
+            else:
+                self.__dataQ.put(newData)
+
+    def query(self):
+        pass
+
+    def viewPreset(self):
+        pass
