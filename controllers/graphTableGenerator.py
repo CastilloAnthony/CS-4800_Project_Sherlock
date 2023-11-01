fc86ab2cccfd64b35097b32db86638d7a00be4de
spangy22 <109324199+spangy22@users.noreply.github.com>
2023-11-01 09:21:01 -0700

    added initialization function and request data function

--- controllers/graphTableGenerator.py
+++ controllers/graphTableGenerator.py
@@ -5,9 +5,31 @@ import seaborn as sns
 from datetime import datetime, timedelta
 import pytz
 import requests
-from server import DBconnectionAgent
+import predictionModel
 
-filename = 'gg'
+class graphTableGenerator:
+    def __init__(self, requestQ, dataQ):
+        self.__requestQ, self.__dataQ = requestQ, dataQ
+
+    def requestData(self, request):
+        self.__requestQ.put(request)
+        time.sleep(0.1)
+        initialDataID = False
+        while self.__dataQ.empty() != True:
+            newData = self.__dataQ.get()
+            print(newData)
+            if newData['id'] == initialDataID:
+                self.__requestQ.put(request)
+                time.sleep(0.1) #import time
+                initialDataID = False
+            elif initialDataID == False:
+                initialDataID = newData['id']
+            if newData['id'] == request['id']:
+                if newData['data'] is not False:
+                    return newData
+            else:
+                self.__dataQ.put(newData)
+
 
 #load files
 #need function to read requested websites from database
