c723df8c2057c5d31e89fbe9e79d1134d576af1e
spangy22 <pangysierra@gmail.com>
2023-11-05 12:53:52 -0800

    changed time conversion function

--- controllers/graphTableGenerator.py
+++ controllers/graphTableGenerator.py
@@ -1,3 +1,4 @@
+#implemented by Sierra
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +7,7 @@ from datetime import datetime, timedelta
 import pytz
 import requests
 import controllers.predictionModel
+
 
 class graphTableGenerator:
     def __init__(self, requestQ, dataQ):
@@ -33,37 +35,23 @@ class graphTableGenerator:
                 self.__dataQ.put(newData)
                 print(len(newData))
 
-            # have the requested data print out in a list
+        
     
     def timeConversion():
         registeredStartDate = datetime()
-        GMT = [] # This our GMT_time 
         for i in datetime: 
             dt = datetime.strptime(i,"%d:%m:%Y %H:%M:%S") # Stripped information from each row 
-            GMT.append(dt)   
             
-        # Convert GMT to Local Time
-
         #PDT_time = []  
         T = pd.Series(data=GMT) # Inputting GMT_time into a series 
-
-        # Converts GMT Dates and Times to Pacific Daylight Savings Time 
-        local_Date_time = T.dt.tz_localize('GMT').dt.tz_convert('America/Los_Angeles').dt.tz_localize(None) # This line converts time_zones
-
         # Storing local dates and times into list  
         PDT_Date_time = []
         for i in local_Date_time: 
             dt_objects = i.to_pydatetime() # Turns the timestamps to datetime objects 
             PDT_Date_time.append(dt_objects)
-            
-        # Find the total seconds starting the week of april 4th 
-
         PDT_time = [] # official PDT time 
-
         for i in PDT_Date_time: # Takes time in 
-            
             timediff = i.timestamp() - registeredStartDate.timestamp()
-            
             PDT_time.append(timediff)
             
         print(PDT_Date_time[0])
